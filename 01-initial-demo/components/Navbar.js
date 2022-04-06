c62694f16f7bff97742828a87c5114e344a6ffc8
ctola91 <ctolapacheco@gmail.com>
2022-04-05 20:47:40 -0400

    add ActiveLink

--- 01-initial-demo/components/Navbar.js
+++ 01-initial-demo/components/Navbar.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import { ActiveLink } from './ActiveLink';
+
+import styles from './Navbar.module.css';
 
 export const Navbar = () => {
   return (
-    <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+    <nav className={styles['menu-container']}>
+        <ActiveLink text="Home" href="/" />
+        <ActiveLink text="About" href="/about" />
+        <ActiveLink text="Contact" href="/contact" />
     </nav>
   )
 }
--- 01-initial-demo/pages/contact/index.js
+++ 01-initial-demo/pages/contact/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Navbar } from '../../components/Navbar'
 
 export default function Contact() {
   return (
@@ -10,7 +11,9 @@ export default function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar />
       <main>
+
         <h1>
           Contact
         </h1>
