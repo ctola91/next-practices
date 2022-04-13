81980e25141c53011a3c2a3d541f2e4a592c5641
ctola91 <ctolapacheco@gmail.com>
2022-04-13 09:32:24 -0400

    add MainLayout

--- 01-initial-demo/pages/about.js
+++ 01-initial-demo/pages/about.js
@@ -1,47 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Navbar } from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import MainLayout from "../components/layouts/MainLayout";
+import { Navbar } from "../components/Navbar";
+import styles from "../styles/Home.module.css";
 
 export default function About() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>About - Christian</title>
-        <meta name="description" content="About page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MainLayout>
+      <h1>About Page</h1>
+      <h1 className={styles.title}>
+        {/* Ir a <a href="/">Home</a> */}
+        Ir a <Link href="/">Home</Link>
+      </h1>
 
-      <Navbar />
-
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <h1 className={styles.title}>
-          {/* Ir a <a href="/">Home</a> */}
-          Ir a <Link href="/">Home</Link>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+      <p className={styles.description}>
+        Get started by editing{" "}
+        <code className={styles.code}>pages/index.js</code>
+      </p>
+    </MainLayout>
+  );
 }
