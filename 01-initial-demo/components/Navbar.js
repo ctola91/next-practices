504a4fae3bc23b43ba44a46a25c5a9e258c8830b
ctola91 <ctolapacheco@gmail.com>
2022-04-13 10:06:42 -0400

    add pricing page and make navbar dynamic

--- 01-initial-demo/components/Navbar.js
+++ 01-initial-demo/components/Navbar.js
@@ -2,12 +2,36 @@ import Link from "next/link";
 import React from "react";
 import { ActiveLink } from "./ActiveLink";
 
+import styles from "./Navbar.module.css";
+
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
+
 export const Navbar = () => {
   return (
-    <nav className="menu-container">
-      <ActiveLink text="Home" href="/" />
+    <nav className={styles["menu-container"]}>
+      {menuItems.map((menuItem) => (
+        <ActiveLink text={menuItem.text} href={menuItem.href} />
+      ))}
+      {/* <ActiveLink text="Home" href="/" />
       <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      <ActiveLink text="Contact" href="/contact" /> */}
     </nav>
   );
 };
